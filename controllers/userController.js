a0f5a94a65e2ac42ccd61cd2b27a4244482e08d5
guitarumoto <guitarumoto@gmail.com>
2024-11-27 16:52:56 -0400

    Criar usuário e login

---
+++ controllers/userController.js
@@ -0,0 +1,38 @@
+const bcrypt = require('bcryptjs');
+const { User } = require('../models');
+
+exports.register = async (req, res) => {
+    const { name, email, password, superUser } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ error: "E-mail e senha são obrigatórios." });
+    }
+
+    try {
+        const existingUser = await User.findOne({ where: { email } });
+        if (existingUser) {
+            return res.status(400).json({ error: "O e-mail já está em uso." });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+            superUser,
+        });
+
+        const userData = {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            superUser: user.superUser,
+        };
+
+        res.status(201).json(userData);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Erro ao registrar o usuário." });
+    }
+};
