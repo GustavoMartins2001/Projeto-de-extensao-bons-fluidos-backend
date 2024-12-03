f7af022dd87db8288b30e56b81f1161d6c12cabf
guitarumoto <guitarumoto@gmail.com>
2024-12-03 17:24:39 -0400

    Compilar codigo

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require('express');
 require('dotenv').config();
-const userRoutes            = require('./routes/userRoutes');
-const authRoutes            = require('./routes/authRoutes');
-const postRoutes            = require('./routes/postRoutes');
-const postInteractionRoutes = require('./routes/postInteractionRoutes');
+const userRoutes = require('./routes/userRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
-app.use('/api/post', postRoutes);
-app.use('/api/post-interaction', postInteractionRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const { register, updateUser } = require('../controllers/userController');
+const { register} = require('../controllers/userController');
 const authenticateToken = require('../middlewares/authenticateToken');
 
 const router = express.Router();
 
 router.post('/register', register);
-router.put('/update', authenticateToken, updateUser);
 
 module.exports = router;
