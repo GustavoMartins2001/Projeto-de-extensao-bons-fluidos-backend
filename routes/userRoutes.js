ef8dffa43de27ba9ce2e304e287a51ea98d864a4
GustavoMartins2001 <gustavoms050@gmail.com>
2024-12-30 18:16:49 -0300

    resolvido erro de permissão ao cadastrar novo usuario

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ const authenticateToken = require('../middlewares/authenticateToken');
 
 const router = express.Router();
 
-router.post('/register', authenticateToken("user"), register);
+router.post('/register', register);
 router.get('/list', authenticateToken("user"), list);
 router.put('/update/:id', authenticateToken("user"), update);
 router.delete('/delete/:id', authenticateToken("user"), destroy);
